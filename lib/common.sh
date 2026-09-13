b95844b305b8e914af775cab1cfcdf3883f97a66
MattyCodes <matt.nick.fagioli@gmail.com>
2026-09-13 14:21:13 -0400

    chore: remove password validation.

--- lib/common.sh
+++ lib/common.sh
@@ -12,7 +12,6 @@ WORKDIR="$HOME/.interview-sandbox"
 KEY_FILE="$WORKDIR/keys/id_ed25519"
 KNOWN_HOSTS_FILE="$WORKDIR/known_hosts"
 TUNNEL_CONTROL_SOCKET="$WORKDIR/tunnel.sock"
-PASSWORD_FILE="$WORKDIR/code-server-password"
 
 SSH_CONFIG_DIR="$HOME/.ssh/config.d"
 SSH_CONFIG_FILE="$SSH_CONFIG_DIR/interview-sandbox.conf"
